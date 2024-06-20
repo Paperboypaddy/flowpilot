79539e97fd685b9960d4baa8dd926b24a7b30c32
Padraic Hoselton <35149493+Paperboypaddy@users.noreply.github.com>
2024-06-19 20:04:42 -0700

    Test Fix again

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -97,7 +97,7 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
   return false;
 }
 
-static int default_fwd_hook(int bus_num, int addr *to_fwd) {
+static int default_fwd_hook(int bus_num, int addr to_fwd) {
   int addr = GET_ADDR(to_fwd);
   int bus_fwd = -1;
 
@@ -214,7 +214,7 @@ static int alloutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
   return true;
 }
 
-static int alloutput_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
+static int alloutput_fwd_hook(int bus_num, int to_fwd) {
   UNUSED(to_fwd);
   int bus_fwd = -1;
 
